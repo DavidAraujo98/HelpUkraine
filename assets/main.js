859d7e1e391c22daea598175f66517b2c1d2c24e
David Araújo <david2araujo5@gmail.com>
2022-03-02 01:38:01 +0000

    Dictionary

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
-function loadEntries(){
-    
+function loadEntries() {
+    var myModal = new bootstrap.Modal(document.getElementById('welcome'), {
+        keyboard: false
+    });
+    var modalToggle = document.getElementById('toggleMyModal') // relatedTarget
+    myModal.show(modalToggle)
 }
