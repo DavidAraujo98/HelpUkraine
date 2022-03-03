cce73ad4f42a52d003484a9611cfb8c950eb4e78
David Araújo <david2araujo5@gmail.com>
2022-03-03 12:24:24 +0000

    Form complete

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,15 +1,26 @@
 function sendData() {
     const url = "https://script.google.com/macros/s/AKfycbzriW940OQTroyyC4WbunLChdgTCogrf9QEMYAfRdddr2_OVuGjsCiU_wGVdCpRnoiD/exec"
 
-    https://www.youtube.com/watch?v=VPI27L_fQC4
-
-    var data = {
-        name: "nome",//document.getElementById("name").value,
-        url: "url" ,//document.getElementById("url").value,
-        desc: "desc",//document.getElementById("desc").value
+    data = {
+        name: document.getElementById("name").value.toString(),
+        url: document.getElementById("url").value.toString(),
+        desc: document.getElementById("desc").value.toString(),
     };
-    fetch(url, { 
-        method: 'POST',
+
+    console.log(data);
+
+    fetch(url, {
+        method: "POST",
+        mode: "no-cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        redirect: "follow",
         body: JSON.stringify(data)
+    }).then(e => {
+        document.getElementById("name").value = "";
+        document.getElementById("url").value = "";
+        document.getElementById("desc").value = "";
     })
 }
